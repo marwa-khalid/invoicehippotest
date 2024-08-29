07a38c8caf7c48f831bd74f021f87b129223f41e
Marwa Khalid <marwa@nemsit.com>
2024-08-30 04:03:03 +0500

    package error resolved

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: "/",
   build: {
+    rollupOptions: {
+      external: [
+        "@yaireo/tagify/dist/react.tagify",
+        "@yaireo/tagify/dist/tagify.css",
+      ],
+    },
     chunkSizeWarningLimit: 3000,
   },
-})
+});
